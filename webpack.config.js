2553ca4417629c1fbb2d5e31aa415b515a6fe6af
Sanjay Maurya <sanjay.maurya@akasaair.com>
2023-10-19 12:33:05 +0530

    removed unwanted codes

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,16 @@
-module.exports = {};
+const path = require('path');
+
+module.exports = {
+  output: {
+    filename: 'main.js', // Rename the main output file
+    path: path.resolve(__dirname, 'build'), // Adjust the output directory as needed
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'], // Your CSS loader configuration
+      },
+    ],
+  },
+};
